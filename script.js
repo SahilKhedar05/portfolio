39e8470c4636eb2953837512075c6a1106a6d080
SahilKhedar05 <sahilkhedar05@gmail.com>
2026-01-09 20:47:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,4 +21,22 @@ window.addEventListener("scroll", () => {
         }
     });
 });
+// ===== SCROLL REVEAL ANIMATION =====
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+    reveals.forEach(el => {
+        const windowHeight = window.innerHeight;
+        const elementTop = el.getBoundingClientRect().top;
+        const revealPoint = 120;
+
+        if (elementTop < windowHeight - revealPoint) {
+            el.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
+
 
